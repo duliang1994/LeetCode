23787e962b5e4d72c5eb73e43525d881c17d7cad
duliang1994 <1186945606@qq.com>
2019-03-19 09:36:00 +0800

    Update Two Sum.cpp

--- 1. Two Sum/Two Sum.cpp
+++ 1. Two Sum/Two Sum.cpp
@@ -1,3 +1,4 @@
+//暴力二重循环
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -13,5 +14,24 @@ public:
             }
         }
        return ans;
+    }
+};
+//map法 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++){
+            if(m.find(target - nums[i]) != m.end()){
+                ans.push_back(m[target - nums[i]]);
+                ans.push_back(i);
+                break;
+            }
+            m[nums[i]] = i;
+            
+        }
+        return ans;
+        
     }
 };
