2353aa2a5558561f53515edacaf146a7dcd22fda
duliang1994 <1186945606@qq.com>
2019-04-18 22:04:14 +0800

    Create String to Integer (atoi).cpp

---
+++ 8. String to Integer (atoi)/String to Integer (atoi).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        int num = 0;
+        int sign = 1;
+        int i = 0;
+        int n = str.size();
+        while(i < n && str[i] == ' ') i++;
+        if(i < n && str[i] == '+'){
+            i++;
+        } else if(i < n && str[i] == '-'){
+            sign = -1;
+            i++;
+        }
+        for(; i < n; i++){
+            if(str[i] < '0' || str[i] > '9') break;
+            if(num > INT_MAX / 10 || (num == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10)){
+                return sign == -1 ? INT_MIN : INT_MAX;
+            }
+            num = num * 10 + (str[i] - '0');
+        }
+        return sign * num;
+    }
+};
